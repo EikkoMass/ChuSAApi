2a655d67a041398b01eab6cdb6e7ed1286481b6a
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-02-16 03:58:13 -0300

    optimizing routing nomenclature and adding delete, update endpoints

---
+++ ApiRoutes.cs
@@ -0,0 +1,11 @@
+namespace ChuSAApi;
+
+public class ApiRoutes
+{
+    public static class Account
+    {
+        public const string FindById = "{id}";
+        public const string DeleteById = "{id}";
+
+    }
+}
--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -5,7 +5,7 @@ using ChuSAApi.Domain.Entities;
 namespace ChuSAApi.Controllers;
 
 [ApiController]
-[Route("api/v1/account/[controller]")]
+[Route("api/v1/account/")]
 public class AccountController : ControllerBase
 {
     private readonly ILogger<AccountController> _logger;
@@ -18,17 +18,48 @@ public class AccountController : ControllerBase
     }
 
     [HttpGet(Name = "GetAccount")]
-    public IActionResult Get()
+    public IActionResult GetAccounts()
     {
         return  Ok(_db.Users.ToList());
     }
     
     [HttpPost]
-    public IActionResult Add(Users user)
+    public IActionResult AddNewAccount(Users user)
     {
         var users = _db.Users.Add(user);
         _db.SaveChanges();
 
         return Ok(users.Entity);
+    }
+    
+    [HttpGet(ApiRoutes.Account.FindById)]
+    public IActionResult GetAccountById(int id)
+    {
+        return Ok(_db.Users.Find(id));
+    }
+    
+    [HttpPut]
+    public IActionResult UpdateAccount(Users user)
+    {
+        _db.Users.Update(user);
+        _db.SaveChanges();
+        
+        return Ok(user);
+    }
+    
+    [HttpDelete(ApiRoutes.Account.DeleteById)]
+    public IActionResult RemoveAccountById(int id)
+    {
+        //TODO verificar se registro do id existe antes de tentar remover
+        // if (_db.Users.Find(id) == null)
+        // {
+        //     return NotFound("Nao foi encontrado o conta pelo id");
+        // }
+        
+        var user = new Users() { Id = id };
+        _db.Users.Remove(user);
+        _db.SaveChanges();
+
+        return Ok("conta deletada com sucesso");
     }
 }
